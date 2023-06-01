c13e7008cdc1dd5d3681958a8e9c40a380c8a9e2
JPesoa1 <josepesoa002@gmail.com>
2023-06-01 12:52:03 +0200

    Add project files.

---
+++ F2GTraining/Models/Nota.cs
@@ -0,0 +1,8 @@
+﻿namespace F2GTraining.Models
+{
+    public class Nota
+    {
+        public string titulo { get; set; }
+        public string cuerpo { get; set; }
+    }
+}
---
+++ F2GTraining/Services/ServiceSQS.cs
@@ -0,0 +1,31 @@
+﻿using Amazon.SQS.Model;
+using Amazon.SQS;
+using Newtonsoft.Json;
+using System.Net;
+using F2GTraining.Models;
+
+namespace F2GTraining.Services
+{
+    public class ServiceSQS
+    {
+        private IAmazonSQS clientSQS;
+        private string UrlQueue;
+
+        public ServiceSQS(IAmazonSQS amazonSQS, IConfiguration configuration)
+        {
+            this.clientSQS = amazonSQS;
+            this.UrlQueue =
+                configuration.GetValue<string>("AWS:SQS:UrlQueue");
+        }
+
+        public async Task SendMessageAsync(Nota nota)
+        {
+            string json = JsonConvert.SerializeObject(nota);
+            SendMessageRequest request =
+                new SendMessageRequest(this.UrlQueue, json);
+            SendMessageResponse response =
+                await this.clientSQS.SendMessageAsync(request);
+            HttpStatusCode statusCode = response.HttpStatusCode;
+        }
+    }
+}
